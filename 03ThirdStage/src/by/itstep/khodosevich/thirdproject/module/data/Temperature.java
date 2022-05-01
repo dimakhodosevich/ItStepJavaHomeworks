6bf873896bb3896d59a95a01a3f6c1258c2050d0
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-05-01 11:21:43 +0300

    Commit from clone

--- 03ThirdStage/src/by/itstep/khodosevich/thirdproject/module/data/Temperature.java
+++ 03ThirdStage/src/by/itstep/khodosevich/thirdproject/module/data/Temperature.java
@@ -1,6 +1,7 @@
 package by.itstep.khodosevich.thirdproject.module.data;
 
 public class Temperature {
+    // hahaha
     public static double translateCelciesIntoFahrenheit(double celcies) {
         return 9/5.0*celcies+32;
     }
