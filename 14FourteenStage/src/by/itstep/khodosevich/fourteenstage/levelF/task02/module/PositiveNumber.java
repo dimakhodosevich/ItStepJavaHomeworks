34ac64697fb96214586ffeb79e8f55f3dc0bfb04
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-05-09 15:01:21 +0300

    Make logic to PositiveNumber class. Test it well.

--- 14FourteenStage/src/by/itstep/khodosevich/fourteenstage/levelF/task02/module/PositiveNumber.java
+++ 14FourteenStage/src/by/itstep/khodosevich/fourteenstage/levelF/task02/module/PositiveNumber.java
@@ -1,14 +1,39 @@
 package by.itstep.khodosevich.fourteenstage.levelF.task02.module;
 
 public class PositiveNumber {
-    private PositiveNumber(){}
-
-    public static int findFirstPositiveElementIndex(int[] array){
-        return -1;
+    private PositiveNumber() {
     }
 
-    public static int findLastPositiveElementIndex(int[] array){
-        return -1;
+    public static int findFirstPositiveElementIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int firstIndex = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                firstIndex = i;
+                return firstIndex;
+            }
+        }
+
+        return firstIndex;
+    }
+
+    public static int findLastPositiveElementIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int lastIndex = -1;
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] > 0) {
+                lastIndex = i;
+                return lastIndex;
+            }
+        }
+
+        return lastIndex;
     }
 
 
