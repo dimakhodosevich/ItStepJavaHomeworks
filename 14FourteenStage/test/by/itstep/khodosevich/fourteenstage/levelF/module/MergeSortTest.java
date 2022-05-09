e272d207e9ad9e2d8c9ad0c5a584cc82acadd473
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-05-10 00:28:04 +0300

    Make autoformatting.

--- 14FourteenStage/test/by/itstep/khodosevich/fourteenstage/levelF/module/MergeSortTest.java
+++ 14FourteenStage/test/by/itstep/khodosevich/fourteenstage/levelF/module/MergeSortTest.java
@@ -1,6 +1,7 @@
 package by.itstep.khodosevich.fourteenstage.levelF.module;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 import static by.itstep.khodosevich.fourteenstage.levelF.module.MergeSort.*;
 
@@ -14,12 +15,12 @@ public class MergeSortTest {
         assertArrayEquals(expected, actual);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void testMergeSortAscNegativeWithZeroLength() {
         mergeSortAsc(new int[0]);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void testMergeSortAscNegativeWithNull() {
         mergeSortAsc(null);
     }
@@ -57,19 +58,19 @@ public class MergeSortTest {
         assertArrayEquals(expected, actual);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void testMergeSortAscNegativeWithErrorStart() {
         int[] actual = {7, 6, 2, 0, 2};
         mergeSortAsc(actual, -1, 3);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void testMergeSortAscNegativeWithErrorEnd() {
         int[] actual = {7, 6, 2, 0, 2};
         mergeSortAsc(actual, 0, 6);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void testMergeSortAscNegativeWithErrorStartEnd() {
         int[] actual = {7, 6, 2, 0, 2};
         mergeSortAsc(actual, 6, 0);
@@ -84,12 +85,12 @@ public class MergeSortTest {
         assertArrayEquals(expected, actual);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void testMergeSortDesNegativeWithZeroLength() {
         mergeSortDes(new int[0]);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void testMergeSortDesNegativeWithNull() {
         mergeSortDes(null);
     }
@@ -127,19 +128,19 @@ public class MergeSortTest {
         assertArrayEquals(expected, actual);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void testMergeSortDesNegativeWithErrorStart() {
         int[] actual = {7, 6, 2, 0, 2};
         mergeSortDes(actual, -1, 3);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void testMergeSortDesNegativeWithErrorEnd() {
         int[] actual = {7, 6, 2, 0, 2};
         mergeSortDes(actual, 0, 6);
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void testMergeSortDesNegativeWithErrorStartEnd() {
         int[] actual = {7, 6, 2, 0, 2};
         mergeSortDes(actual, 6, 0);
