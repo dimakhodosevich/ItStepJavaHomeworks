99de324b5d3439f7c20719be8a655aefe210d443
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-05-14 22:31:19 +0300

    Make controller for task05

--- 14FourteenStage/src/by/itstep/khodosevich/fourteenstage/levelF/task05/controller/ControllerLevelFTask05.java
+++ 14FourteenStage/src/by/itstep/khodosevich/fourteenstage/levelF/task05/controller/ControllerLevelFTask05.java
@@ -1,12 +1,14 @@
-package by.itstep.khodosevich.fourteenstage.levelF.task04;
+package by.itstep.khodosevich.fourteenstage.levelF.task05.controller;
 
-import by.itstep.khodosevich.fourteenstage.levelF.module.*;
+import by.itstep.khodosevich.fourteenstage.levelF.module.Array;
+import by.itstep.khodosevich.fourteenstage.levelF.module.NegativeNumber;
+import by.itstep.khodosevich.fourteenstage.levelF.module.SelectSort;
 import by.itstep.khodosevich.fourteenstage.view.Printer;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ControllerLevelFTask04 {
+public class ControllerLevelFTask05 {
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(System.in);
@@ -15,8 +17,8 @@ public class ControllerLevelFTask04 {
             int[] array = Array.initArray(size);
             Printer.print("Your array is: " + Arrays.toString(array));
             Printer.print("\nWhich type of action do you want to do?");
-            Printer.print("\n1. Sort array by insert sort method (descend) to the first positive element;");
-            Printer.print("\n2. Sort array by insert sort method (descend) to the last positive element.");
+            Printer.print("\n1. Sort array by select sort method (ascend) to the first negative element;");
+            Printer.print("\n2. Sort array by select sort method (ascend) to the last negative element.");
             Printer.print("\nPush only first of second number!!!\n");
 
             int variant;
@@ -29,30 +31,30 @@ public class ControllerLevelFTask04 {
                 }
             }
 
-            String message1 = "Your array doesn't has positive element!!!";
-            String message2 = "Positive element has index ";
+            String message1 = "Your array doesn't has negative element!!!";
+            String message2 = "Negative element has index ";
 
             if (variant == 1) {
-                int firstIndex = PositiveNumber.findFirstPositiveElementIndex(array);
+                int firstIndex = NegativeNumber.findFirstNegativeElementIndex(array);
 
                 if (firstIndex == -1) {
                     Printer.printError(message1);
                 } else {
                     message2 += firstIndex;
                     Printer.print(message2);
-                    InsertSort.insertSortDes(array, 0, firstIndex - 1);
+                    SelectSort.selectSortAsc(array, 0, firstIndex - 1);
                     Printer.print("\n" + Arrays.toString(array));
                 }
 
             } else {
-                int lastIndex = PositiveNumber.findLastPositiveElementIndex(array);
+                int lastIndex = NegativeNumber.findLastNegativeElementIndex(array);
 
                 if (lastIndex == -1) {
                     Printer.printError(message1);
                 } else {
                     message2 += lastIndex;
                     Printer.print(message2);
-                    InsertSort.insertSortDes(array, 0, lastIndex - 1);
+                    SelectSort.selectSortAsc(array, 0, lastIndex - 1);
                     Printer.print("\n" + Arrays.toString(array));
                 }
             }
