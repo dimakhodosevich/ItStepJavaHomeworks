dc630939044025a9607f69947c1ff29d65bbf93f
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-05-15 21:22:47 +0300

    Make ControllerLevelETask01. Change some logic at sum method - delete start>=end.

--- 14FourteenStage/src/by/itstep/khodosevich/fourteenstage/levelE/module/Sum.java
+++ 14FourteenStage/src/by/itstep/khodosevich/fourteenstage/levelE/module/Sum.java
@@ -5,7 +5,7 @@ public class Sum {
     }
 
     public static double sum(double[] array, int start, int end) {
-        if (array == null || array.length == 0 || start < 0 || end > array.length || start >= end) {
+        if (array == null || array.length == 0 || start < 0 || end > array.length) {
             throw new RuntimeException();
         }
 
---
+++ 14FourteenStage/src/by/itstep/khodosevich/fourteenstage/levelE/task01/controller/ControllerLevelETask01.java
@@ -0,0 +1,40 @@
+package by.itstep.khodosevich.fourteenstage.levelE.task01.controller;
+
+
+import by.itstep.khodosevich.fourteenstage.levelE.module.Array;
+import by.itstep.khodosevich.fourteenstage.levelE.module.MaxAndMinIndex;
+import by.itstep.khodosevich.fourteenstage.levelE.module.Sum;
+import by.itstep.khodosevich.fourteenstage.view.Printer;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ControllerLevelETask01 {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            Printer.print("Input size of your array: ");
+            int size = scanner.nextInt();
+            double[] array = Array.initArray(size);
+            Printer.print("Your array is: " + Arrays.toString(array));
+            int minIndex = MaxAndMinIndex.getMinElementIndex(array);
+            int maxIndex = MaxAndMinIndex.getMaxElementIndex(array);
+            double max = array[maxIndex];
+            double min = array[minIndex];
+
+            Printer.print("\nMax value " + max + ", has index " + maxIndex);
+            Printer.print("\nMin value " + min + ", has index " + minIndex);
+
+            double sum = maxIndex > minIndex
+                    ? Sum.sum(array, minIndex + 1, maxIndex)
+                    : Sum.sum(array, maxIndex + 1, minIndex);
+
+            Printer.print("\nThe sum between max and min element is " + sum);
+
+        } catch (Exception exception) {
+            Printer.print("\nSomething went wrong!!! Look at stack trace.");
+            Printer.print(exception.getStackTrace());
+        }
+    }
+
+}
